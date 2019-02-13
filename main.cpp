8f2fc70a54470e75ffffe7dd6006f33559051698
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-02-13 17:56:23 -0600

    Update main.cpp
    FizzBuzz

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main(){
 	
 	for(int n=1; n<101; n++){
-		if (n%3=0){
+		if (n%3==0){
 			cout << "Fizz" << endl;
-		}if (n%5=0){
+		}else if (n%5==0){
 			cout << "Buzz" << endl;
-		}if (n%15=0){
+		}else if (n%15==0){
 			cout << "FizzBuzz" << endl;
 		}else{
 			cout << n << endl;
