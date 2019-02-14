b51036364cfb1a21a2bcdfcce4df314642b2dc34
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-02-13 18:44:01 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main(){
 	
 	for(int n=1; n<101; n++){
-		if (n%3==0){
-			cout << "Fizz" << endl;
+		if  (n%15==0){
+			cout << "FizzBuzz" << endl;
 		}else if (n%5==0){
 			cout << "Buzz" << endl;
-		}else if (n%15==0){
-			cout << "FizzBuzz" << endl;
+		}else if (n%3== 0){
+			cout << "Fizz" << endl;
 		}else{
 			cout << n << endl;
 		}
